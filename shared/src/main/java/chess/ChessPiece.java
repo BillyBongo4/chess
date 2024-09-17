cf709b28d162407e0af62235bd68ac76bf43acaf
Spencer Hill <spencerthefencer10@hotmail.com>
2024-09-17 09:28:27 -0600

    Finished Queen and Rook

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -113,6 +113,107 @@ public class ChessPiece {
                 rowMod--;
             }
         }
+        else if (type == PieceType.KNIGHT) { System.out.println("Temp!"); }
+        else if (type == PieceType.PAWN) { System.out.println("Temp!"); }
+        else if (type == PieceType.QUEEN) {
+            for (int i = 0; i < 4; i++) {
+                int modifier = 1;
+                while (true) {
+                    int rowMod = modifier;
+                    int colMod = modifier;
+                    if (i == 1 || i == 3) { rowMod *= -1; }
+                    if (i == 2 || i == 3) { colMod *= -1; }
+
+                    ChessPosition currPosition = new ChessPosition(myPosition.getRow() + rowMod,
+                            myPosition.getColumn() + colMod);
+
+                    if (currPosition.getRow() > 8 || currPosition.getRow() < 1) { break; }
+                    if (currPosition.getColumn() > 8 || currPosition.getColumn() < 1) { break; }
+                    if (board.getPiece(currPosition) != null) {
+                        if (board.getPiece(currPosition).getTeamColor() == getTeamColor()) {
+                            break;
+                        }
+                    }
+
+                    ChessMove move = new ChessMove(myPosition, currPosition, null);
+                    moves.add(move);
+                    modifier++;
+
+                    if (board.getPiece(currPosition) != null) {
+                        if (board.getPiece(currPosition).getTeamColor() != getTeamColor()) {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int modifier = 1;
+                while (true) {
+                    int rowMod = modifier;
+                    int colMod = modifier;
+                    if (i == 1 || i == 3) { rowMod = 0; }
+                    else if (i == 2) { rowMod *= -1; }
+                    if (i == 0 || i == 2) { colMod = 0; }
+                    else if (i == 3) { colMod *= -1; }
+
+                    ChessPosition currPosition = new ChessPosition(myPosition.getRow() + rowMod,
+                            myPosition.getColumn() + colMod);
+
+                    if (currPosition.getRow() > 8 || currPosition.getRow() < 1) { break; }
+                    if (currPosition.getColumn() > 8 || currPosition.getColumn() < 1) { break; }
+                    if (board.getPiece(currPosition) != null) {
+                        if (board.getPiece(currPosition).getTeamColor() == getTeamColor()) {
+                            break;
+                        }
+                    }
+
+                    ChessMove move = new ChessMove(myPosition, currPosition, null);
+                    moves.add(move);
+                    modifier++;
+
+                    if (board.getPiece(currPosition) != null) {
+                        if (board.getPiece(currPosition).getTeamColor() != getTeamColor()) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        else if (type == PieceType.ROOK) {
+            for (int i = 0; i < 4; i++) {
+                int modifier = 1;
+                while (true) {
+                    int rowMod = modifier;
+                    int colMod = modifier;
+                    if (i == 1 || i == 3) { rowMod = 0; }
+                    else if (i == 2) { rowMod *= -1; }
+                    if (i == 0 || i == 2) { colMod = 0; }
+                    else if (i == 3) { colMod *= -1; }
+
+                    ChessPosition currPosition = new ChessPosition(myPosition.getRow() + rowMod,
+                            myPosition.getColumn() + colMod);
+
+                    if (currPosition.getRow() > 8 || currPosition.getRow() < 1) { break; }
+                    if (currPosition.getColumn() > 8 || currPosition.getColumn() < 1) { break; }
+                    if (board.getPiece(currPosition) != null) {
+                        if (board.getPiece(currPosition).getTeamColor() == getTeamColor()) {
+                            break;
+                        }
+                    }
+
+                    ChessMove move = new ChessMove(myPosition, currPosition, null);
+                    moves.add(move);
+                    modifier++;
+
+                    if (board.getPiece(currPosition) != null) {
+                        if (board.getPiece(currPosition).getTeamColor() != getTeamColor()) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
         return moves;
     }
 
