cde9940add1a1f9f6b1b887a0e6355a3950af9d8
Spencer Hill <spencerthefencer10@hotmail.com>
2024-09-17 15:05:49 -0600

    First implementation of knight moves.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -113,7 +113,44 @@ public class ChessPiece {
                 rowMod--;
             }
         }
-        else if (type == PieceType.KNIGHT) { System.out.println("Temp!"); }
+        else if (type == PieceType.KNIGHT) {
+            for (int i = 0; i < 4; i++) {
+                int rowMod = 2;
+                int colMod = 1;
+
+                if (i == 1 || i == 3) {
+                    rowMod--;
+                    colMod++;
+
+                    if (i == 3) { colMod *= -1; }
+                }
+                else if (i == 2) { rowMod *= -1; }
+
+                for (int j = 0; j < 2; j++) {
+                    if ((i == 0 || i == 2) && j == 1) { colMod *= -1; }
+                    else if ((i == 1 || i == 3) && j == 1) { rowMod *= -1; }
+
+                    ChessPosition currPosition = new ChessPosition(myPosition.getRow() + rowMod,
+                            myPosition.getColumn() + colMod);
+
+                    boolean validSpace = true;
+                    if (currPosition.getRow() > 8 || currPosition.getRow() < 1) { validSpace = false; }
+                    if (currPosition.getColumn() > 8 || currPosition.getColumn() < 1) { validSpace = false; }
+
+
+                    if (board.getPiece(currPosition) != null) {
+                        if (board.getPiece(currPosition).getTeamColor() == getTeamColor()) {
+                            validSpace = false;
+                        }
+                    }
+
+                    if (validSpace) {
+                        ChessMove move = new ChessMove(myPosition, currPosition, null);
+                        moves.add(move);
+                    }
+                }
+            }
+        }
         else if (type == PieceType.PAWN) { System.out.println("Temp!"); }
         else if (type == PieceType.QUEEN) {
             for (int i = 0; i < 4; i++) {
