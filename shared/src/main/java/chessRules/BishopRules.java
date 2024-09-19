9a437e08f58684b939bda63234b472c653e6da65
Spencer Hill <spencerthefencer10@hotmail.com>
2024-09-18 21:59:18 -0600

    BishopRules.java : changed moves to pieceMoves. QueenRules.java : implemented pieceMoves function.

--- shared/src/main/java/chessRules/BishopRules.java
+++ shared/src/main/java/chessRules/BishopRules.java
@@ -8,7 +8,7 @@ import chess.ChessPosition;
 import java.util.Collection;
 
 public class BishopRules extends Rules {
-    public Collection<ChessMove> moves(ChessPiece currPiece, ChessPosition myPosition, ChessBoard board) {
+    public Collection<ChessMove> pieceMoves(ChessPiece currPiece, ChessPosition myPosition, ChessBoard board) {
         super.diagonalMoves(currPiece, myPosition, board);
         return getMoves();
     }
--- shared/src/main/java/chessRules/QueenRules.java
+++ shared/src/main/java/chessRules/QueenRules.java
@@ -1,5 +1,16 @@
 package chessRules;
 
-public class QueenRules extends Rules {
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
+import java.util.Collection;
+
+public class QueenRules extends Rules {
+    public Collection<ChessMove> pieceMove(ChessPiece currPiece, ChessPosition myPosition, ChessBoard board) {
+        super.diagonalMoves(currPiece, myPosition, board);
+        super.straightMoves(currPiece, myPosition, board);
+        return getMoves();
+    }
 }
