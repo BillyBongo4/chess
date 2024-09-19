db6112e00b811d20d1dc859bc8ecf8a13b4acc2f
Spencer Hill <spencerthefencer10@hotmail.com>
2024-09-18 22:22:13 -0600

    Initialized

---
+++ shared/src/main/java/chessRules/KnightRules.java
@@ -0,0 +1,14 @@
+package chessRules;
+
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Collection;
+
+public class KnightRules extends Rules {
+    public Collection<ChessMove> pieceMove(ChessPiece currPiece, ChessPosition myPosition, ChessBoard board) {
+        return getMoves();
+    }
+}
