c59d68c0b6ab803705e17d1f1bc0b522a4a0caac
Spencer Hill <spencerthefencer10@hotmail.com>
2024-09-30 10:35:11 -0600

    Added removePiece function to ChessBoard.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -25,6 +25,10 @@ public class ChessBoard {
         chessBoard[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
+    public void removePiece(ChessPosition position) {
+        chessBoard[position.getRow() - 1][position.getColumn() - 1] = null;
+    }
+
     /**
      * Gets a chess piece on the chessboard
      *
