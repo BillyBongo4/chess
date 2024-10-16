0fd6c8cc3d6f5117b898c07c37077fa98b1c2573
Spencer Hill <spencerthefencer10@hotmail.com>
2024-10-16 10:36:12 -0600

    Added MemoryDataAccess object and RegisterService object.

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,11 +1,17 @@
 import chess.*;
+import dataaccess.MemoryDataAccess;
 import server.Server;
+import service.RegisterService;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
-        Server server = new Server();
+
+        MemoryDataAccess memoryDataAccess = new MemoryDataAccess();
+        RegisterService registerService = new RegisterService(memoryDataAccess);
+        Server server = new Server(registerService);
+        
         server.run(8080);
     }
 }
