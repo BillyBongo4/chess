ca802233e6104eadd231414d0d6569ad8b4b8d21
Spencer Hill <spencerthefencer10@hotmail.com>
2024-10-16 12:25:10 -0600

    Added database access variable and created constructor.

--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,4 +1,11 @@
 package service;
 
+import dataaccess.DataAccess;
+
 public class LoginService {
+    private final DataAccess dataAccess;
+
+    public LoginService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
 }
