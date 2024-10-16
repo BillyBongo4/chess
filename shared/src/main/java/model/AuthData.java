6e46b85fca7b92c880a5e5e02b46b176e26814e4
Spencer Hill <spencerthefencer10@hotmail.com>
2024-10-16 12:31:24 -0600

    Created constructor that takes in username.

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,6 +1,13 @@
 package model;
 
+import java.util.UUID;
+
 public class AuthData {
     private String authToken;
     private String username;
+
+    public AuthData(String username) {
+        authToken = UUID.randomUUID().toString();
+        this.username = username;
+    }
 }
