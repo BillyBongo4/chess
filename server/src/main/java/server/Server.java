1f91a06651e7c808e0969473d57c12556ede8e1f
Spencer Hill <spencerthefencer10@hotmail.com>
2024-10-16 09:58:02 -0600

    Added register handler and register method.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,7 @@
 package server;
 
+import com.google.gson.Gson;
+import model.UserData;
 import spark.*;
 
 public class Server {
@@ -10,12 +12,19 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", this::register);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private Object register(Request request, Response response) {
+        var user = new Gson().fromJson(request.body(), UserData.class);
+        
+        return new Gson().toJson(user);
     }
 
     public void stop() {
