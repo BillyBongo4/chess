9c04fb39f463ae0578b5274ee1452c93b22fa198
Spencer Hill <spencerthefencer10@hotmail.com>
2024-10-16 10:26:44 -0600

    Created getUser, createUser and createAuth methods.

--- server/src/main/java/service/RegisterService.java
+++ server/src/main/java/service/RegisterService.java
@@ -1,5 +1,20 @@
 package service;
 
+import dataaccess.DataAccess;
+import model.*;
+
 public class RegisterService {
-    
+    private final DataAccess dataAccess;
+
+    public RegisterService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public UserData getUser(String username) {
+        return dataAccess.getUser(username);
+    }
+
+    public UserData createUser(UserData userData) {
+        return dataAccess.createUser(userData);
+    }
 }
