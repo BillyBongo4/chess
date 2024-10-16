26cb600cbc6673848f806522a1df3f14c96d23b1
Spencer Hill <spencerthefencer10@hotmail.com>
2024-10-16 10:32:46 -0600

    Added constructor and getUsername method.

--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -4,4 +4,12 @@ public class UserData {
     private String username;
     private String password;
     private String email;
+
+    UserData(String username) {
+        this.username = username;
+    }
+
+    String getUsername() {
+        return username;
+    }
 }
