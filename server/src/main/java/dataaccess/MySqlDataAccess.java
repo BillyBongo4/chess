3d6c0c30088fbb53dcbf8cc1b6eb3d78eddfb7e1
Spencer Hill <spencerthefencer10@hotmail.com>
2024-10-28 16:58:40 -0600

    Added createTables function, and started implementing getUser.

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -2,44 +2,93 @@ package dataaccess;
 
 import model.*;
 
+import java.sql.SQLException;
+
 public class MySqlDataAccess implements DataAccess {
+    public MySqlDataAccess() {
+        try {
+            DatabaseManager.createDatabase();
+            createTables();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void createTables() throws Exception {
+        String userDataTable = "CREATE TABLE IF NOT EXISTS users (" +
+                "username VARCHAR(50) NOT NULL PRIMARY KEY, " +
+                "password VARCHAR(50) NOT NULL, " +
+                "email VARCHAR(100) NOT NULL)";
+        String authDataTable = "CREATE TABLE IF NOT EXISTS auths (" +
+                "authToken VARCHAR(255) NOT NULL PRIMARY KEY, " +
+                "username VARCHAR(50) NOT NULL)";
+        String gameDataTable = "CREATE TABLE IF NOT EXISTS games (" +
+                "gameID INT AUTO_INCREMENT PRIMARY KEY, " +
+                "gameName VARCHAR(100) NOT NULL, " +
+                "whiteUsername VARCHAR(50), " +
+                "blackUsername VARCHAR(50), " +
+                "chessGame TEXT NOT NULL)";
+
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var statement = conn.createStatement()) {
+                statement.executeUpdate(userDataTable);
+                statement.executeUpdate(authDataTable);
+                statement.executeUpdate(gameDataTable);
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
     @Override
-    public UserData getUser(String username) {
+    public UserData getUser(String username) throws Exception {
+        String query = "SELECT * FROM users WHERE username = ?";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(query)) {
+                preparedStatement.setString(1, username);
+                var rs = preparedStatement.executeQuery();
+                if (rs.next()) {
+                    return new UserData(rs.getString("username"), rs.getString("password"), rs.getString("email"));
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
         return null;
     }
 
     @Override
-    public UserData createUser(UserData userData) {
+    public UserData createUser(UserData userData) throws DataAccessException {
         return null;
     }
 
     @Override
-    public AuthData createAuth(AuthData authData) {
+    public AuthData createAuth(AuthData authData) throws Exception {
         return null;
     }
 
     @Override
-    public AuthData getAuth(String authToken) {
+    public AuthData getAuth(String authToken) throws DataAccessException {
         return null;
     }
 
     @Override
-    public void deleteAuth(String authToken) {
+    public void deleteAuth(String authToken) throws DataAccessException {
 
     }
 
     @Override
-    public GameData[] listGames() {
+    public GameData[] listGames() throws DataAccessException {
         return new GameData[0];
     }
 
     @Override
-    public GameData createGame(GameData gameData) {
+    public GameData createGame(String gameName) throws DataAccessException {
         return null;
     }
 
     @Override
-    public GameData getGame(int gameID) {
+    public GameData getGame(int gameID) throws DataAccessException {
         return null;
     }
 
@@ -49,7 +98,7 @@ public class MySqlDataAccess implements DataAccess {
     }
 
     @Override
-    public void updateGame(int gameID, String username, String color) {
+    public void updateGame(int gameID, String username, String color) throws DataAccessException {
 
     }
 
