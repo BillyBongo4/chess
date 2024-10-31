c5b6b56630e9d1dd0e5a31d5f036ed46973618eb
Spencer Hill <spencerthefencer10@hotmail.com>
2024-10-31 16:51:10 -0600

    Changed methods to throw DataAccessException and added parameter validation.

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -19,13 +19,27 @@ public class MemoryDataAccess implements DataAccess {
     }
 
     @Override
-    public UserData createUser(UserData userData) {
+    public UserData createUser(UserData userData) throws DataAccessException {
+        if (userData.username() == null || userData.username().isEmpty()) {
+            throw new DataAccessException("Error: Invalid username");
+        } else if (userData.password() == null || userData.password().isEmpty()) {
+            throw new DataAccessException("Error: Invalid password");
+        } else if (userData.email() == null || userData.email().isEmpty()) {
+            throw new DataAccessException("Error: Invalid email");
+        }
+
         users.put(userData.username(), userData);
         return userData;
     }
 
     @Override
-    public AuthData createAuth(AuthData authData) throws Exception {
+    public AuthData createAuth(AuthData authData) throws DataAccessException {
+        if (authData.authToken() == null || authData.authToken().isEmpty()) {
+            throw new DataAccessException("Error: Invalid authToken");
+        } else if (authData.username() == null || authData.username().isEmpty()) {
+            throw new DataAccessException("Error: Invalid username");
+        }
+
         auths.put(authData.authToken(), authData);
         return authData;
     }
@@ -46,7 +60,11 @@ public class MemoryDataAccess implements DataAccess {
     }
 
     @Override
-    public GameData createGame(String gameName) {
+    public GameData createGame(String gameName) throws DataAccessException {
+        if (gameName == null || gameName.isEmpty()) {
+            throw new DataAccessException("Error: Invalid gameName");
+        }
+
         GameData gameData = new GameData(games.size() + 1, null, null, gameName, new ChessGame());
         games.put(gameData.gameId(), gameData);
         return gameData;
@@ -67,7 +85,15 @@ public class MemoryDataAccess implements DataAccess {
     }
 
     @Override
-    public void updateGame(int gameID, String username, String color) {
+    public void updateGame(int gameID, String username, String color) throws DataAccessException {
+        if (gameID > listGames().length) {
+            throw new DataAccessException("Error: Invalid gameID");
+        } else if (username.isEmpty()) {
+            throw new DataAccessException("Error: Invalid username");
+        } else if (!color.equals("WHITE") && !color.equals("BLACK")) {
+            throw new DataAccessException("Error: Invalid color");
+        }
+
         String whiteUsername = games.get(gameID).whiteUsername();
         String blackUsername = games.get(gameID).blackUsername();
         String gameName = games.get(gameID).gameName();
