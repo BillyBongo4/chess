99876a407f70c0a6a639ec53942fbca9e7e70446
Spencer Hill <spencerthefencer10@hotmail.com>
2024-11-26 14:13:56 -0700

    Started implementing NotificationHandler

--- client/src/main/java/websocket/NotificationHandler.java
+++ client/src/main/java/websocket/NotificationHandler.java
@@ -1,4 +1,30 @@
 package websocket;
 
+import websocket.messages.Notification;
+import websocket.messages.ServerMessage;
+
 public class NotificationHandler {
+    public void handleNotification(ServerMessage message) {
+        if (message.getClass() == Notification.class) {
+            handleNotificationMessage((Notification) message);
+        } else {
+            handleOtherMessageTypes(message);
+        }
+    }
+
+    private void handleNotificationMessage(Notification message) {
+        System.out.println("Notification: " + message.getMessage());
+    }
+
+    private void handleOtherMessageTypes(ServerMessage message) {
+        switch (message.getServerMessageType()) {
+            case LOAD_GAME:
+                break;
+            case ERROR:
+                break;
+            case NOTIFICATION:
+                handleNotificationMessage((Notification) message);
+                break;
+        }
+    }
 }
