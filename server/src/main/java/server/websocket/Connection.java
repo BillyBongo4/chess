114d1f9c83d01ada576db2924ce96472b098996a
Spencer Hill <spencerthefencer10@hotmail.com>
2024-11-26 14:30:43 -0700

    Implemented Connection and ConnectionManager.

--- server/src/main/java/server/websocket/Connection.java
+++ server/src/main/java/server/websocket/Connection.java
@@ -1,4 +1,29 @@
 package server.websocket;
 
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.io.IOException;
+
 public class Connection {
+    private String username;
+    private Session session;
+
+    public Connection(String username, Session session) {
+        this.username = username;
+        this.session = session;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void send(String message) throws IOException {
+        if (session != null && session.isOpen()) {
+            session.getRemote().sendString(message);
+        }
+    }
 }
--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,4 +1,38 @@
 package server.websocket;
 
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.Notification;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ConnectionManager {
+    private final Map<String, Connection> connections = new ConcurrentHashMap<>();
+
+    public void addConnection(String username, Session session) {
+        connections.put(username, new Connection(username, session));
+    }
+
+    public void removeConnection(String username) {
+        connections.remove(username);
+    }
+
+    public void broadcast(String excludingUsername, Notification message) throws IOException {
+        var removeList = new ArrayList<Connection>();
+        for (var connection : connections.values()) {
+            if (connection.getSession().isOpen()) {
+                if (!connection.getUsername().equals(excludingUsername)) {
+                    connection.send(message.toString());
+                }
+            } else {
+                removeList.add(connection);
+            }
+        }
+
+        for (var connection : removeList) {
+            connections.remove(connection.getUsername());
+        }
+    }
 }
