a235786829502dc87ab83cd16a1923bb1e0c4c81
Spencer Hill <spencerthefencer10@hotmail.com>
2024-11-26 14:01:58 -0700

    Implemented websocket facade.

--- client/src/main/java/websocket/WebSocketFacade.java
+++ client/src/main/java/websocket/WebSocketFacade.java
@@ -1,4 +1,46 @@
 package websocket;
 
-public class WebSocketFacade {
+import com.google.gson.Gson;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WebSocketFacade extends Endpoint {
+    private Session session;
+    private NotificationHandler notificationHandler;
+
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws IOException, URISyntaxException, DeploymentException {
+        url.replace("http", "ws");
+        URI socketURI = new URI(url + "/ws");
+        this.notificationHandler = notificationHandler;
+
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        this.session = container.connectToServer(this, socketURI);
+
+        this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+            @Override
+            public void onMessage(String message) {
+                ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
+                notificationHandler.notify(serverMessage);
+            }
+        });
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+
+    public void sendCommand(UserGameCommand command) throws IOException {
+        this.session.getBasicRemote().sendText(new Gson().toJson(command));
+    }
+
+    public void closeSession() throws IOException {
+        if (this.session != null && this.session.isOpen()) {
+            this.session.close();
+        }
+    }
 }
