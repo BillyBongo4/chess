db9b9fc1440ba790a54584aeb084f6f06339a91d
Spencer Hill <spencerthefencer10@hotmail.com>
2024-11-26 09:28:38 -0700

    Started implementation

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,5 +1,35 @@
 package server.websocket;
 
-public class WebSocketHandler {
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.UserGameCommand;
 
+@WebSocket
+public class WebSocketHandler {
+    private ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws Exception {
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        switch (command.getCommandType()) {
+            case CONNECT -> handleConnect(session, command);
+            case MAKE_MOVE -> handleMakeMove(command);
+            case RESIGN -> handleResign(command);
+            case LEAVE -> handleLeave(command);
+        }
+    }
+
+    private void handleConnect(Session session, UserGameCommand command) {
+    }
+
+    private void handleMakeMove(UserGameCommand command) {
+    }
+
+    private void handleResign(UserGameCommand command) {
+    }
+
+    private void handleLeave(UserGameCommand command) {
+    }
 }
