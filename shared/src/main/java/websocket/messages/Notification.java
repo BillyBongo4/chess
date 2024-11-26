c4d8e8e458975228f2dfc03f768158504579d4a0
Spencer Hill <spencerthefencer10@hotmail.com>
2024-11-25 17:37:08 -0700

    Implemented Notification.

--- shared/src/main/java/websocket/messages/Notification.java
+++ shared/src/main/java/websocket/messages/Notification.java
@@ -1,4 +1,37 @@
 package websocket.messages;
 
-public class Notification {
+import java.util.Objects;
+
+public class Notification extends ServerMessage {
+    private final String message;
+
+    public Notification(String message) {
+        super(ServerMessageType.NOTIFICATION);
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (this.getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+
+        Notification notification = (Notification) o;
+        return Objects.equals(getMessage(), notification.getMessage());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getMessage());
+    }
 }
