983f590347bfbb499ca86be90047bf43e1b5c627
Spencer Hill <spencerthefencer10@hotmail.com>
2024-11-25 17:30:29 -0700

    Implemented Makemove

--- shared/src/main/java/websocket/commands/MakeMove.java
+++ shared/src/main/java/websocket/commands/MakeMove.java
@@ -1,4 +1,39 @@
 package websocket.commands;
 
-public class MakeMove {
+import chess.ChessMove;
+
+import java.util.Objects;
+
+public class MakeMove extends UserGameCommand {
+    private ChessMove move;
+
+    public MakeMove(String authToken, Integer gameID, ChessMove move) {
+        super(CommandType.MAKE_MOVE, authToken, gameID);
+        this.move = move;
+    }
+
+    public ChessMove getMove() {
+        return move;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (this.getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+
+        MakeMove makeMove = (MakeMove) o;
+        return Objects.equals(getMove(), makeMove.getMove());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getMove());
+    }
 }
