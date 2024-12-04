268dee5abfc599556098ac600023c0a2d56d075d
Spencer Hill <spencerthefencer10@hotmail.com>
2024-12-04 10:04:01 -0700

    Simplified the code

--- client/src/main/java/websocket/NotificationHandler.java
+++ client/src/main/java/websocket/NotificationHandler.java
@@ -5,18 +5,6 @@ import websocket.messages.ServerMessage;
 
 public class NotificationHandler {
     public void handleNotification(ServerMessage message) {
-        if (message.getClass() == Notification.class) {
-            handleNotificationMessage((Notification) message);
-        } else {
-            handleOtherMessageTypes(message);
-        }
-    }
-
-    private void handleNotificationMessage(Notification message) {
-        System.out.println("Notification: " + message.getMessage());
-    }
-
-    private void handleOtherMessageTypes(ServerMessage message) {
         switch (message.getServerMessageType()) {
             case LOAD_GAME:
                 System.out.println("TEST");
@@ -24,7 +12,8 @@ public class NotificationHandler {
             case ERROR:
                 break;
             case NOTIFICATION:
-                handleNotificationMessage((Notification) message);
+                Notification notification = (Notification) message;
+                System.out.println("Notification: " + notification);
                 break;
         }
     }
