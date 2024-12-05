707972c6d02a851c8af9f7bc24ab5a218d25399f
Spencer Hill <spencerthefencer10@hotmail.com>
2024-12-05 10:59:09 -0700

    Got redraw and validMoves to work

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -15,6 +15,7 @@ import javax.websocket.DeploymentException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Arrays;
+import java.util.Collection;
 
 import static ui.EscapeSequences.*;
 
@@ -27,6 +28,7 @@ public class Client {
     private String color;
     private int gameID = 0;
     private boolean observing = false;
+    private ChessGame game = null;
     private final WebSocketFacade ws;
     //private final NotificationHandler notificationHandler;
 
@@ -49,6 +51,8 @@ public class Client {
                 case "list" -> list();
                 case "join" -> join(params);
                 case "observe" -> observe(params);
+                case "redraw" -> redrawBoard();
+                case "valid" -> validMoves(params);
                 case "move" -> makeMove(params);
                 case "resign" -> resign();
                 case "leave" -> leave();
@@ -160,6 +164,43 @@ public class Client {
         return output.toString();
     }
 
+    private String buildBoard(ChessGame game, boolean isBlack, ChessPosition selectedPiecePosition, Collection<ChessMove> validMoves) {
+        StringBuilder output = new StringBuilder();
+        int startRow = isBlack ? 0 : 7;
+        int endRow = isBlack ? 8 : -1;
+        int rowStep = isBlack ? 1 : -1;
+        int startCol = isBlack ? 7 : 0;
+        int endCol = isBlack ? -1 : 8;
+        int colStep = isBlack ? -1 : 1;
+
+        for (int i = startRow; i != endRow; i += rowStep) {
+            output.append(SET_BG_COLOR_DARK_GREY).append(" ").append(i + 1).append(" ");
+            for (int j = startCol; j != endCol; j += colStep) {
+                ChessPosition currentPosition = new ChessPosition(i + 1, j + 1);
+                String bgColor;
+                if (currentPosition.equals(selectedPiecePosition)) {
+                    bgColor = SET_BG_COLOR_YELLOW;
+                } else if (validMoves.stream().anyMatch(move -> move.getEndPosition().equals(currentPosition))) {
+                    bgColor = SET_BG_COLOR_GREEN;
+                } else {
+                    bgColor = ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) ? SET_BG_COLOR_BLACK : SET_BG_COLOR_WHITE;
+                }
+                ChessPiece currPiece = game.getBoard().getPiece(currentPosition);
+                if (currPiece != null) {
+                    String textColor = (currPiece.getTeamColor() == ChessGame.TeamColor.WHITE) ? SET_TEXT_COLOR_BLUE : SET_TEXT_COLOR_RED;
+                    String pieceSymbol = getPieceSymbol(currPiece);
+                    output.append(bgColor).append(textColor).append(" ").append(pieceSymbol).append(" ");
+                } else {
+                    output.append(bgColor).append("   ");
+                }
+                output.append(RESET_TEXT_COLOR);
+            }
+            output.append(SET_BG_COLOR_DARK_GREY).append(" ").append(i + 1).append(" ").append(RESET_BG_COLOR).append("\n");
+        }
+        return output.toString();
+    }
+
+
     private String outputBoard(ChessGame game, String... params) {
         StringBuilder output = new StringBuilder();
         String labels = params[1].equals("black") ? "h  g  f  e  d  c  b  a" : "a  b  c  d  e  f  g  h";
@@ -175,14 +216,29 @@ public class Client {
         return output.toString();
     }
 
+    private String outputBoard(ChessGame game, ChessPosition position, Collection<ChessMove> validMoves, String... params) {
+        StringBuilder output = new StringBuilder();
+        String labels = params[1].equals("black") ? "h  g  f  e  d  c  b  a" : "a  b  c  d  e  f  g  h";
+
+        output.append(RESET_TEXT_COLOR);
+
+        output.append(buildHeaderFooter(labels));
+        output.append(buildBoard(game, params[1].equals("black"), position, validMoves));
+        output.append(buildHeaderFooter(labels));
+
+        output.append(RESET_BG_COLOR);
+        return output.toString();
+    }
+
     public String join(String... params) throws Exception {
         try {
             if (params.length == 2) {
-                var game = server.joinGame(authToken, params[0], params[1]);
+                game = server.joinGame(authToken, params[0], params[1]);
                 ws.sendCommand(new Connect(authToken, Integer.parseInt(params[0]), username, params[1]));
                 color = params[1];
                 gameID = Integer.parseInt(params[0]);
-                return outputBoard(game, params);
+                return "";
+                //return outputBoard(game, params);
             }
             return "Expected: join <ID> <WHITE|BLACK>";
         } catch (Exception e) {
@@ -201,11 +257,24 @@ public class Client {
             ws.sendCommand(new Connect(authToken, Integer.parseInt(params[0]), username, "observer"));
             color = "observer";
             observing = true;
-            var game = server.observeGame(authToken, params[0]);
-            return outputBoard(game, newParams);
+            game = server.observeGame(authToken, params[0]);
+            return "";
+            //return outputBoard(game, newParams);
         } catch (Exception e) {
             throw new Exception("Invalid id!");
         }
+    }
+
+    public String redrawBoard() throws Exception {
+        game = server.observeGame(authToken, String.valueOf(gameID));
+        return outputBoard(game, String.valueOf(gameID), color);
+    }
+
+    public String validMoves(String... params) throws Exception {
+        game = server.observeGame(authToken, String.valueOf(gameID));
+        ChessPosition position = parsePosition(params[0]);
+        Collection<ChessMove> validMoves = game.validMoves(position);
+        return outputBoard(game, position, validMoves, String.valueOf(gameID), color);
     }
 
     private ChessPosition parsePosition(String pos) {
@@ -216,11 +285,12 @@ public class Client {
 
     public String makeMove(String... params) throws Exception {
         if (params.length == 2) {
+            game = server.observeGame(authToken, String.valueOf(gameID));
             ChessMove move = new ChessMove(parsePosition(params[0]), parsePosition(params[1]), null);
-            ChessGame game = server.observeGame(authToken, String.valueOf(gameID));
             MakeMove moveCommand = new MakeMove(authToken, gameID, move, game, color);
             ws.sendCommand(moveCommand);
-            return "Move sent";
+            //game = server.observeGame(authToken, String.valueOf(gameID));
+            return "";
         }
         throw new IOException("Expected: <source> <destination>");
     }
@@ -232,6 +302,7 @@ public class Client {
         gameID = 0;
         color = "";
         observing = false;
+        game = null;
         return String.format("%s left the game", username);
     }
 
@@ -251,6 +322,7 @@ public class Client {
             gameID = 0;
             color = null;
             observing = false;
+            game = null;
             ws.closeSession();
             return "Logged out successfully!";
         }
