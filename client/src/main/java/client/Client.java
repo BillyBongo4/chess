c0246eed389671bd1f931032c3156b654a95b9bc
Spencer Hill <spencerthefencer10@hotmail.com>
2024-12-06 18:45:02 -0700

    Combined methods to comply with code quality requirements.

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -32,11 +32,10 @@ public class Client {
     private WebSocketFacade ws;
     private final NotificationHandler notificationHandler;
 
-    public Client(String serverUrl, NotificationHandler notificationHandler) throws IOException, URISyntaxException, DeploymentException {
+    public Client(String serverUrl, NotificationHandler notificationHandler) {
         server = new ServerFacade(serverUrl);
         this.serverUrl = serverUrl;
         this.notificationHandler = notificationHandler;
-        //ws = new WebSocketFacade(serverUrl, new NotificationHandler());
     }
 
     public String eval(String input) {
@@ -130,34 +129,6 @@ public class Client {
         return SET_BG_COLOR_DARK_GREY + "    " + labels + "    " + RESET_BG_COLOR + "\n";
     }
 
-    private String buildBoard(ChessGame game, boolean isBlack) {
-        StringBuilder output = new StringBuilder();
-        int startRow = isBlack ? 0 : 7;
-        int endRow = isBlack ? 8 : -1;
-        int rowStep = isBlack ? 1 : -1;
-        int startCol = isBlack ? 7 : 0;
-        int endCol = isBlack ? -1 : 8;
-        int colStep = isBlack ? -1 : 1;
-
-        for (int i = startRow; i != endRow; i += rowStep) {
-            output.append(SET_BG_COLOR_DARK_GREY).append(" ").append(i + 1).append(" ");
-            for (int j = startCol; j != endCol; j += colStep) {
-                String bgColor = ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) ? SET_BG_COLOR_BLACK : SET_BG_COLOR_WHITE;
-                ChessPiece currPiece = game.getBoard().getPiece(new ChessPosition(i + 1, j + 1));
-                if (currPiece != null) {
-                    String textColor = (currPiece.getTeamColor() == ChessGame.TeamColor.WHITE) ? SET_TEXT_COLOR_BLUE : SET_TEXT_COLOR_RED;
-                    String pieceSymbol = getPieceSymbol(currPiece);
-                    output.append(bgColor).append(textColor).append(" ").append(pieceSymbol).append(" ");
-                } else {
-                    output.append(bgColor).append("   ");
-                }
-                output.append(RESET_TEXT_COLOR);
-            }
-            output.append(SET_BG_COLOR_DARK_GREY).append(" ").append(i + 1).append(" ").append(RESET_BG_COLOR).append("\n");
-        }
-        return output.toString();
-    }
-
     private String buildBoard(ChessGame game, boolean isBlack, ChessPosition selectedPiecePosition, Collection<ChessMove> validMoves) {
         StringBuilder output = new StringBuilder();
         int startRow = isBlack ? 0 : 7;
@@ -172,9 +143,9 @@ public class Client {
             for (int j = startCol; j != endCol; j += colStep) {
                 ChessPosition currentPosition = new ChessPosition(i + 1, j + 1);
                 String bgColor;
-                if (currentPosition.equals(selectedPiecePosition)) {
+                if (selectedPiecePosition != null && currentPosition.equals(selectedPiecePosition)) {
                     bgColor = SET_BG_COLOR_YELLOW;
-                } else if (validMoves.stream().anyMatch(move -> move.getEndPosition().equals(currentPosition))) {
+                } else if (validMoves != null && validMoves.stream().anyMatch(move -> move.getEndPosition().equals(currentPosition))) {
                     bgColor = SET_BG_COLOR_GREEN;
                 } else {
                     bgColor = ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) ? SET_BG_COLOR_BLACK : SET_BG_COLOR_WHITE;
@@ -194,7 +165,6 @@ public class Client {
         return output.toString();
     }
 
-
     public String outputBoard(ChessGame game, String... params) {
         StringBuilder output = new StringBuilder();
         String labels = params[1].equals("black") ? "h  g  f  e  d  c  b  a" : "a  b  c  d  e  f  g  h";
@@ -203,7 +173,7 @@ public class Client {
 
         output.append(buildHeaderFooter(labels));
         //output.append(buildBoard(game, !params[1].equals("black")));
-        output.append(buildBoard(game, params[1].equals("black")));
+        output.append(buildBoard(game, params[1].equals("black"), null, null));
         output.append(buildHeaderFooter(labels));
 
         output.append(RESET_BG_COLOR);
@@ -247,8 +217,6 @@ public class Client {
 
     public String observe(String... params) throws Exception {
         try {
-            /*String[] newParams = Arrays.copyOf(params, params.length + 1);
-            newParams[newParams.length - 1] = "white";*/
             ws = new WebSocketFacade(serverUrl, notificationHandler);
             ws.sendCommand(new Connect(authToken, Integer.parseInt(params[0]), username));
             gameID = Integer.parseInt(params[0]);
@@ -256,7 +224,6 @@ public class Client {
             observing = true;
             game = server.observeGame(authToken, params[0]);
             return "";
-            //return outputBoard(game, newParams);
         } catch (Exception e) {
             throw new Exception("Invalid id!");
         }
